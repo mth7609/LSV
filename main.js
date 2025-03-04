66be934989477db8ae5cbc5dd530ed50a7edf5b8
Michael Thiele <michael.thiele@wangiro.de>
2025-03-04 01:04:56 +0100

    ...

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
 const path = require('node:path')
 
 
@@ -8,15 +8,23 @@ const createWindow = () => {
     height: 800,
   })
   win.loadFile('index.html');
-  }
+}
 
+const createDebugWindow = () => {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+  })
+  win.loadFile('debug.html');
+}
 
-  app.whenReady().then(() => {
+app.whenReady().then(() => {
   createWindow();
-  
+  //createDebugWindow();
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createWindow();
+      //createDebugWindow();
     }
   })
 })
@@ -28,3 +36,7 @@ app.on('window-all-closed', () => {
 }
 
 )
+
+// Build EXE in C:\Projects\Electron\LSV>
+// electron - packager.Archiv--overwrite--asar = true --platform = win32--arch = ia32--icon = assets / icons / win / icon.ico--prune = true --out = release - builds--version - string.CompanyName=CE--version - string.FileDescription=CE--version - string.ProductName="Archive"
+// Result in c:\Projects\Electron\LSV\release-builds\
