42852e7df986bca36aad4856eaa0e3c75aa39f69
Michael Thiele <michael.thiele@wangiro.de>
2025-03-05 23:09:34 +0100

    ...

--- scripts.js
+++ scripts.js
@@ -1,6 +1,3 @@
-
-//alert("jjjj");
-
 var selectedDropdown = 0;
 var publisherIs = "";
 let searchCnt = 1;
@@ -10,21 +7,44 @@ maxReached = false;
 
 changeRange(searchCnt);
 
-const searchItems = Array.from({ length: maxSearchSets+1 }, () => new Array(elementsOnForm).fill(0));
+const searchItems = Array.from({ length: maxSearchSets + 1 }, () => new Array(elementsOnForm).fill(0));
 
-$(".dropdown-menu li a").click(updateValue);
-$(".doSearch").click(doSearch);
-$("#dropdown-state").click(sel1);
-$("#dropdown-year").click(sel2);
-$("#btnradio1").click(publisherSchool);
-$("#btnradio2").click(publisherFree);
-$(".r1").click(updateRange);
+$(".dropdown-menu li a").on('click', updateValue);
+$(".doSearch").on('click', doSearch);
+$("#dropdown-state").on('click', sel1);
+$("#dropdown-year").on('click', sel2);
+$("#btnradio1").on('click', publisherSchool);
+$("#btnradio2").on('click', publisherFree);
+$(".r1").on('click', updateRange);
+
+
 
 function updateRange(str) {
 
+    //console.log(searchItems[$(".r1").val()][4]);
+
+    document.getElementById('dropdown-state').innerText = searchItems[$(".r1").val()][3];
+    document.getElementById('dropdown-year').innerText = searchItems[$(".r1").val()][5];
+
+    if (searchItems[$(".r1").val()][4] == "Frei") {
+        $('#btnradio1').prop("checked", false);
+        $('#btnradio2').prop("checked", true);
+    }
+    else
+        if (searchItems[$(".r1").val()][4] == "Schule") {
+            $('#btnradio1').prop("checked", true);
+            $('#btnradio2').prop("checked", false);
+        }
+        else {
+            $('#btnradio1').prop("checked", false);
+            $('#btnradio2').prop("checked", false);
+        }
+
 
     document.getElementById('name').value = searchItems[$(".r1").val()][0];
-
+    document.getElementById('schoolPublisher').value = searchItems[$(".r1").val()][1];
+    document.getElementById('city').value = searchItems[$(".r1").val()][2];
+    document.getElementById('no').value = searchItems[$(".r1").val()][6];
 }
 
 function changeRange(str) {
@@ -81,17 +101,13 @@ function doSearch() {
     $('#year-Read').text(searchItems[searchCnt][5]);
     $('#no-Read').text(searchItems[searchCnt][6]);
     $('#searchCnt').text(searchCnt);
-    $('#debug').text($(".form-range").val());
 
-
-    if ((searchCnt < maxSearchSets))
-    {
-        if( !maxReached)
+    if ((searchCnt < maxSearchSets)) {
+        if (!maxReached)
             changeRange(searchCnt);
         searchCnt = searchCnt + 1;
     }
-    else
-    {
+    else {
         maxReached = true;
         searchCnt = 1;
     }
