1855c618cf53d3c252e8d82bb9b3beab3a76e86f
Michael Thiele <michael.thiele@wangiro.de>
2025-05-16 04:10:23 +0200

    temp

--- Sources_last/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron/renderer')
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  openSearchProcess: () => ipcRenderer.send('openSearchProcessCMD'),
-  closeSearchProcess: () => ipcRenderer.send('closeSearchProcessCMD'),
-  closeMainProcess: () => ipcRenderer.send('closeMainProcessCMD'),
-  getHttpPort: (callback) => ipcRenderer.on('httpPort', (_event, value) => callback(value)),
-})
-
